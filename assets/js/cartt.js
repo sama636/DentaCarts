5a35cfb8fa6a5c8e72d07b7d5054db164616d1fe
sama636 <snasser@636gmail.com>
2023-09-12 19:39:20 +0300

    init commit

--- assets/js/cartt.js
+++ assets/js/cartt.js
@@ -1,131 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const RestorativeDiv = document.querySelector(".RestorativeDiv");
-// const Restorative = document.querySelector(".Restorative");
-//     Restorative.addEventListener("mouseover", function () {
-//         RestorativeDiv.style.display = "block";
-//     });
-//     Restorative.addEventListener("mouseout", function () {
-//         RestorativeDiv.style.display = "none";
-//     });
-//     RestorativeDiv.addEventListener("mouseover", function () {
-//         RestorativeDiv.style.display = "block";
-//     });
-//     RestorativeDiv.addEventListener("mouseout", function () {
-//         RestorativeDiv.style.display = "none";
-//     });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//     const EndodonticsDiv = document.querySelector(".EndodonticsDiv");
-// const Endodontics = document.querySelector(".Endodontics");
-// Endodontics.addEventListener("mouseover", function () {
-//     EndodonticsDiv.style.display = "block";
-//     });
-//     Endodontics.addEventListener("mouseout", function () {
-//         EndodonticsDiv.style.display = "none";
-//     });
-//     EndodonticsDiv.addEventListener("mouseover", function () {
-//         EndodonticsDiv.style.display = "block";
-//     });
-//     EndodonticsDiv.addEventListener("mouseout", function () {
-//         EndodonticsDiv.style.display = "none";
-//     });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//     const OrthodonticsDiv = document.querySelector(".OrthodonticsDiv");
-// const Orthodontics = document.querySelector(".Orthodontics");
-// Orthodontics.addEventListener("mouseover", function () {
-//     OrthodonticsDiv.style.display = "block";
-//     });
-//     Orthodontics.addEventListener("mouseout", function () {
-//         OrthodonticsDiv.style.display = "none";
-//     });
-//     OrthodonticsDiv.addEventListener("mouseover", function () {
-//         OrthodonticsDiv.style.display = "block";
-//     });
-//     OrthodonticsDiv.addEventListener("mouseout", function () {
-//         OrthodonticsDiv.style.display = "none";
-//     });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//     const ImplantDiv = document.querySelector(".ImplantDiv");
-// const Implant = document.querySelector(".Implant");
-// Implant.addEventListener("mouseover", function () {
-//     ImplantDiv.style.display = "block";
-//     });
-//     Implant.addEventListener("mouseout", function () {
-//         ImplantDiv.style.display = "none";
-//     });
-//     ImplantDiv.addEventListener("mouseover", function () {
-//         ImplantDiv.style.display = "block";
-//     });
-//     ImplantDiv.addEventListener("mouseout", function () {
-//         ImplantDiv.style.display = "none";
-//     });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//     const PerioDiv = document.querySelector(".PerioDiv");
-// const Perio = document.querySelector(".Perio");
-// Perio.addEventListener("mouseover", function () {
-//     PerioDiv.style.display = "block";
-//     });
-//     Perio.addEventListener("mouseout", function () {
-//         PerioDiv.style.display = "none";
-//     });
-//     PerioDiv.addEventListener("mouseover", function () {
-//         PerioDiv.style.display = "block";
-//     });
-//     PerioDiv.addEventListener("mouseout", function () {
-//         PerioDiv.style.display = "none";
-//     });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//     const ConsumablesDiv = document.querySelector(".ConsumablesDiv");
-// const Consumables = document.querySelector(".Consumables");
-// Consumables.addEventListener("mouseover", function () {
-//     ConsumablesDiv.style.display = "block";
-//     });
-//     Consumables.addEventListener("mouseout", function () {
-//         ConsumablesDiv.style.display = "none";
-//     });
-//     ConsumablesDiv.addEventListener("mouseover", function () {
-//         ConsumablesDiv.style.display = "block";
-//     });
-//     ConsumablesDiv.addEventListener("mouseout", function () {
-//         ConsumablesDiv.style.display = "none";
-//     });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//     const InstrmentDiv = document.querySelector(".InstrmentDiv");
-// const Instrment = document.querySelector(".Instrment");
-// Instrment.addEventListener("mouseover", function () {
-//     InstrmentDiv.style.display = "block";
-//     });
-//     Instrment.addEventListener("mouseout", function () {
-//         InstrmentDiv.style.display = "none";
-//     });
-//     InstrmentDiv.addEventListener("mouseover", function () {
-//         InstrmentDiv.style.display = "block";
-//     });
-//     InstrmentDiv.addEventListener("mouseout", function () {
-//         InstrmentDiv.style.display = "none";
-//     });
-// });
-// document.addEventListener("DOMContentLoaded", function () {
-//     const EquipmentDiv = document.querySelector(".EquipmentDiv");
-// const Equipment = document.querySelector(".Equipment");
-// Equipment.addEventListener("mouseover", function () {
-//     EquipmentDiv.style.display = "block";
-//     });
-//     Equipment.addEventListener("mouseout", function () {
-//         EquipmentDiv.style.display = "none";
-//     });
-//     EquipmentDiv.addEventListener("mouseover", function () {
-//         EquipmentDiv.style.display = "block";
-//     });
-//     EquipmentDiv.addEventListener("mouseout", function () {
-//         EquipmentDiv.style.display = "none";
-//     });
-// });
+
 // Moblile Nav
 const restorativeArrow = document.querySelector(".restorativeArrow")
 const restorativeArrowDown = document.querySelector(".restorativeArrowDown")
@@ -156,8 +29,9 @@ restorativeArrowDown.addEventListener("click",function(){
 let cart = JSON.parse(localStorage.getItem('cart'));
 
 const prodCont = document.querySelector('.cart-cont .content');
+const priceTotal = document.querySelector(".price-total")
 
-displayData(cart)
+displayData(cart);
 function displayData(ele){
     const cart = JSON.parse(localStorage.getItem('cart'));
     let elements=""
@@ -185,8 +59,10 @@ function displayData(ele){
     </div> <hr>`
     })
     prodCont.innerHTML = elements;
+    calcTotal();
+    addEventsToBtn()
+
 }
-addEventsToBtn()
 function addEventsToBtn(){
     const increasBtnd = document.querySelectorAll('.itemIcons .fa-plus')
     const dereasBtnd = document.querySelectorAll('.itemIcons .fa-minus');
@@ -204,6 +80,7 @@ function addEventsToBtn(){
             const itemquantity = parent.querySelector(".itemquantity");
             itemquantity.textContent = prodObj.quantity;
             localStorage.setItem('cart', JSON.stringify(cart))
+            calcTotal()
         })
         
     })
@@ -217,6 +94,7 @@ function addEventsToBtn(){
             const itemquantity = parent.querySelector(".itemquantity");
             itemquantity.textContent = prodObj.quantity;
             localStorage.setItem('cart', JSON.stringify(cart))
+            calcTotal()
         })
         
     })
@@ -230,13 +108,15 @@ function addEventsToBtn(){
             console.log(cart);
             par.remove();
             localStorage.setItem('cart', JSON.stringify(cart));
+            calcTotal()
         })
     })
 
 }
-const btn = document.querySelector(".check");
-function btnCheck(){
-    btn.addEventListener('click', () =>{
-        alert("checkout");
-    })
+
+function calcTotal(){
+    let total = cart.reduce((acc, ele) =>{
+        return acc + (+ele.price * ele.quantity);
+    } , 0)
+    console.log(total);
 }
